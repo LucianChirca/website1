e18b013c53cc7ff04f83a0cbd1a32c50a63c1eb8
s1976427 <l.chirca@student.utwente.nl>
2019-12-17 14:35:02 +0100

    Updates

---
+++ precache-manifest.3ee2ca087d0dc32b9f57547021f7e660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08f162569976e47cfe5d8ccc656874f",
+    "url": "/website1/index.html"
+  },
+  {
+    "revision": "b7487fb8ba555453e41b",
+    "url": "/website1/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "479da068bef82a7098aa",
+    "url": "/website1/static/js/2.f4a73aca.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/website1/static/js/2.f4a73aca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7487fb8ba555453e41b",
+    "url": "/website1/static/js/main.cd758b71.chunk.js"
+  },
+  {
+    "revision": "b5846e95c321bb248fa0",
+    "url": "/website1/static/js/runtime-main.6052ae03.js"
+  }
+]);
